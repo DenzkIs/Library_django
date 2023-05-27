ac7d8a0f22c15665d75ebdc476c4618b67cdef4c
DenIs <pydenzk@gmail.com>
2023-05-27 13:08:07 +0300

    Added links from main page

--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import get_main_page
+from .views import *
 
 urlpatterns = [
-    path('main/', get_main_page, name='main_page')
+    path('main/', get_main_page, name='main_page'),
+    path('new_book/', get_new_book, name='new_book_page'),
+    path('new_reader/', get_new_reader, name='new_reader_page'),
+    path('lend_book/', get_lend_book, name='lend_book_page'),
+    path('return_book/', get_return_book, name='return_book_page'),
+
 ]
--- library/views.py
+++ library/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def get_main_page(request):
     return render(request, 'main.html')
+
+
+def get_new_book(request):
+    return render(request, 'new_book.html')
+
+
+def get_new_reader(request):
+    return render(request, 'new_reader.html')
+
+
+def get_lend_book(request):
+    return render(request, 'lend_book.html')
+
+
+def get_return_book(request):
+    return render(request, 'return_book.html')
