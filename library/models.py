a8c42c2fe121c9fb6fd1b90321466e3a8d55c9fe
DenIs <pydenzk@gmail.com>
2023-05-28 22:46:46 +0300

    Added some models, without migrations yet

--- library/models.py
+++ library/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Author name')
+    foto = models.ImageField(upload_to='images/authors', verbose_name="Author's foto",
+                             default='images/authors/author_default.png')
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Genre name')
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title_rus = models.CharField(max_length=255, verbose_name='Russian title')
+    title_original = models.CharField(max_length=255, verbose_name='Original title', blank=True)
+    genre = models.ManyToManyField(Genre)
+    cost = models.DecimalField(default=0, max_digits=5, decimal_places=2, verbose_name='Cost (BYN)')
+    quantity = models.IntegerField(default=0, verbose_name='Number of books')
+    author = models.ManyToManyField(Author)
+    rent_day = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Rental per day (BYN)')
+    year = models.IntegerField(blank=True, verbose_name='The year of published')
+    date_reg = models.DateTimeField(auto_now_add=True, verbose_name='Date of registration')
+    page = models.IntegerField(blank=True, verbose_name='Number of pages')
+
+    def __str__(self):
+        return f'{self.id}: {self.title_rus}'
+
+
+class BookCover(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to='images/covers', verbose_name="Cover's foto",
+                             default='images/covers/cover_default.jpg')
+
+    def __str__(self):
+        return 'Cover for book'
